3c1df6e6bd3ecea2709db80001a2e1c16d5c3d75
AnuvaA <aambasth@redhat.com>
2019-10-03 10:26:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 echo "This is a simple web application";
 echo "<br>";
 echo "This is a simple web application";
-echo "<br>The correct response to the survey is 5 stars";
+echo "<br>The correct response to the survey is 10 stars";
