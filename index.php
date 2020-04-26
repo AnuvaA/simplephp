25367a23e581de92fcf21aab2359599ba2c20a60
AnuvaA <aambasth@redhat.com>
2020-04-27 00:20:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "This is a simple web application";
+echo "This is a simple test web application";
 echo "<br>";
 echo "This is a simple Great web application";
 echo "<br>The correct response to the survey is 20 stars";
